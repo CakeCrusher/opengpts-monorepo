8196873c0e4fe3c745e5221755ce4a2c558d96b7
Sebastian Sosa <1sebastian1sosa1@gmail.com>
2024-01-16 18:00:03 -0500

    run steps endpoint

--- backend/app/routers/thread_router.py
+++ backend/app/routers/thread_router.py
@@ -1,5 +1,12 @@
+"""
+TESTING
+asst_UOmnQLhM5Yf1jiZx56J7bleW
+thread_6XCcv97SvXjmSqPXXdmXz1mf
+6e9b3cb6-5ba5-4457-abcb-e0d08546c6ae
+"""
+
 import os
-from typing import List
+from typing import Dict, List
 from fastapi import APIRouter, Depends, HTTPException
 from openai import OpenAI
 from utils.parsers import get_user_id
@@ -8,6 +15,7 @@ from dotenv import load_dotenv
 from sqlalchemy.orm import Session
 from db import crud, schemas
 from models.thread import (
+    CreateThreadMessageResponse,
     CustomThread,
     CreateThreadMessage,
     ThreadMessage,
@@ -190,3 +198,100 @@ def get_thread_messages(
         thread_id=thread_id,
     )
     return messages
+
+
+@router.post(
+    "/gpt/{gpt_id}/thread/{thread_id}/run",
+    response_model=CreateThreadMessageResponse,
+)
+def create_thread_run(
+    gpt_id: str,
+    thread_id: str,
+    request: CreateThreadMessage,
+    user_id: str = Depends(get_user_id),
+    db: Session = Depends(get_db),
+):
+    """
+    Create a message in a thread.
+
+    Args:
+    - request (CreateThreadMessage): The message to create.
+
+    Headers:
+    - auth (str): Bearer <USER_ID>
+
+    Returns:
+    - SyncCursorPage[ThreadMessage]: The message history including the
+      assistant response.
+    """
+    client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=request.content,
+    )
+    run = client.beta.threads.runs.create(
+        thread_id=thread_id,
+        assistant_id=gpt_id,
+    )
+
+    max_wait_iterations = 20  # (max_wait_iterations / 2) = seconds to wait
+    i = 0
+    while i < max_wait_iterations:
+        i += 1
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread_id,
+            run_id=run.id,
+        )
+
+        run_steps = client.beta.threads.runs.steps.list(
+            thread_id=thread_id,
+            run_id=run.id,
+        )
+        messages: Dict[str, ThreadMessage] = {}
+
+        # Iterate over the data in run_steps
+        for step in run_steps:
+            # Check if the step type is 'message_creation'
+            if not (
+                step.status == "in_progress"
+                or step.status == "queued"
+                or step.status == "completed"
+            ):
+                raise HTTPException(
+                    status_code=500,
+                    detail="GPT run failed. With status: " + run.status,
+                )
+            if step.type == 'message_creation':
+                # Retrieve the message
+                message = client.beta.threads.messages.retrieve(
+                    thread_id=step.thread_id,
+                    message_id=step.step_details.message_creation.message_id,
+                )
+                # Map the message to the message_id in the hash map
+                messages[
+                    step.step_details.message_creation.message_id
+                ] = message.model_dump()
+                # Pydandtic does not recognize the ThreadMessage object,
+                # so it must be serialized
+
+        json_run_steps = run_steps.model_dump()
+        if len(json_run_steps["data"]) and any(
+            [
+                step["type"] == "message_creation"
+                and step["status"] == "completed"
+                for step in json_run_steps["data"]
+            ]
+        ):
+            break
+
+        time.sleep(0.5)
+    else:
+        raise HTTPException(
+            status_code=500,
+            detail="GPT run failed. With status: " + run.status,
+        )
+
+    return CreateThreadMessageResponse(
+        messages=messages,
+        run_steps=run_steps,
+    )
