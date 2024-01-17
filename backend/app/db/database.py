08574e98434aff01e2368833d51ea2a99696c43b
Sebastian Sosa <1sebastian1sosa1@gmail.com>
2024-01-16 22:39:27 -0500

    postgres updates

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,10 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://devuser:devpass@postgres:5432/devdb"
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
