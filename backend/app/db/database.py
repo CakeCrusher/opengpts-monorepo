b67e95215e71c769fce30905299ae910efca371b
Sebastian Sosa <1sebastian1sosa1@gmail.com>
2024-01-18 21:15:32 -0500

    database and requirements

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -3,9 +3,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
 
-engine = create_engine(DATABASE_URL)
+databse_url = (
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@"
+    + f"{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+)
+
+engine = create_engine(databse_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
